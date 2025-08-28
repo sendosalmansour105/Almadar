a8b2b7dd18f3a2fdf301e6ec1275b2876a2443ea
sendos105 <seses0319@gmail.com>
2025-08-28 23:37:58 +0300

    villa animation

---
+++ assets/js/villa.js
@@ -0,0 +1,43 @@
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+let carouselDom = document.querySelector('.carousel');
+let listitemDom = document.querySelector('.carousel .list');
+let thunailDom = document.querySelector('.carousel .thunbnail');
+
+nextDom.onclick = function () {
+    showSlider("next")
+}
+prevDom.onclick = function () {
+    showSlider("prev")
+}
+let timeRunning = 3000;
+let timeAutoNext = 7000;
+let runTimeOut;
+let runAutoRun = setTimeout(() => {
+    nextDom.click();
+}, timeAutoNext);
+function showSlider(type) {
+    let itemSlider = document.querySelectorAll(".carousel .list .item");
+    let itemthunail = document.querySelectorAll(".carousel .thunbnail .item");
+
+    if (type === "next") {
+        listitemDom.appendChild(itemSlider[0]);
+        thunailDom.appendChild(itemthunail[0]);
+        carouselDom.classList.add('next')
+    }
+    else {
+        let positionlastitem = itemSlider.length - 1;
+        listitemDom.prepend(itemSlider[positionlastitem]);
+        thunailDom.prepend(itemthunail[positionlastitem]);
+        carouselDom.classList.add('prev')
+    }
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove('next')
+        carouselDom.classList.remove('prev');
+    }, timeRunning)
+    clearTimeout(runAutoRun);
+    runAutoRun = setTimeout(() => {
+        nextDom.click();
+    }, timeAutoNext)
+}
